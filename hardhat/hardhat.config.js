bfff85b640dc3bcd4a9e7e3cd2cf413c916afc73
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-27 17:09:05 +0530

    added baseSepolia

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,4 +1,42 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.23",
+  networks: {
+    baseSepolia: {
+      url: process.env.BASE_SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 84532,
+      // gasPrice: "auto",
+      // gas: 5000000,
+      timeout: 10000,
+      confirmations: 2,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      baseSepolia: process.env.BASE_SEPOLIA_API_KEY,
+    },
+    customChains: [
+      {
+        network: "baseSepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: true,
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
 };
