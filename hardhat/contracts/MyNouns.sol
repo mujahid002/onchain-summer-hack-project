942597b295d6d7cab2aea41df1de9d137a8a7497
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-27 17:08:30 +0530

    updated with naming & mint function

--- hardhat/contracts/MyNouns.sol
+++ hardhat/contracts/MyNouns.sol
@@ -1,18 +1,32 @@
 // SPDX-License-Identifier: mujahid002
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact mujahidshaik2002@gmail.com
-contract MyNounTokens is ERC721, Ownable {
+contract MyNouns is ERC721, Ownable {
+    uint256 private s_nounId;
+
     constructor() ERC721("MyNounTokens", "MNT") Ownable(_msgSender()) {
-        for (uint256 i = 0; i < 100; ++i) {
+        s_nounId = 10;
+        for (uint256 i = 0; i < 10; ++i) {
             _safeMint(_msgSender(), i);
         }
     }
 
+    function mintNouns(address to, uint256 amount) public onlyOwner {
+        s_nounId += amount;
+        for (uint256 i = 0; i < amount; ++i) {
+            _safeMint(to, i);
+        }
+    }
+
+    function getTokenId() public view returns (uint256) {
+        return s_nounId;
+    }
+
     function _baseURI() internal pure override returns (string memory) {
-        return "For Demo Testing!";
+        return "For Testing Only!";
     }
 }
